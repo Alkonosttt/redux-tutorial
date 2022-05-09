40b3c1c921becba12a99068e732530b94793dd28
Alkonosttt <voodoomon@protonmail.com>
2022-05-09 16:19:58 +0200

    added the increment and decrement buttons

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import {store} from './app/store';
 import {Provider} from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
